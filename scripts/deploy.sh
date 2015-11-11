099eebe6403fff372813cdc9c24a460513b0d7c8
Grady, Brandon <gradymke@gmail.com>
2015-11-11 11:54:37 -0700

    Use gnu-tar rather than builtin tar

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# This requires that gnu-tar is required on your system.
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../_publish
-tar -cvf site.tar .
+gtar -cvf site.tar .
 gzip site.tar
 
 scp site.tar.gz begrady@dogsbuttbrew.com:/home/begrady/public_html
